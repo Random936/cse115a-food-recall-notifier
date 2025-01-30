9b267bf58ec0c4e168f13c20891ce68bcd84263b
Kenshinobi4 <kenshinobi4@gmail.com>
2025-01-29 23:02:30 -0800

    Added base project from .NET Maui and added a notification channel and using plugin LocalNofifications to simulate a notification.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Plugin.LocalNotification;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1) {
+                var request = new NotificationRequest
+                {
+                    NotificationId = 1131,
+                    Title = "Did it work?",
+                    Subtitle = "I hope it does",
+                    Description = "Yes!",
+                    BadgeNumber = 1,
+                    //Schedule = new NotificationRequestSchedule //to keep it repeating
+                    //{
+                    //    NotifyTime = DateTime.Now.AddSeconds(1)
+                    //    //NotifyRepeatInterval = TimeSpan.FromDays(1),
+                    //}
+                };
+                LocalNotificationCenter.Current.Show(request);
+                Console.WriteLine("Tapped Notification");
+
+
+                CounterBtn.Text = $"Clicked {count} tdsadsaime";
+            }
+            else { 
+                var request = new NotificationRequest
+                {
+                    NotificationId = 1132,
+                    Title = "Yes",
+                    Subtitle = "YES",
+                    Description = "YES!!!",
+                    BadgeNumber = 2,
+                    //Schedule = new NotificationRequestSchedule //to keep it repeating
+                    //{
+                    //    NotifyTime = DateTime.Now.AddSeconds(1)
+                    //    //NotifyRepeatInterval = TimeSpan.FromDays(1),
+                    //}
+                };
+            LocalNotificationCenter.Current.Show(request);
+            CounterBtn.Text = $"Clicked {count} times";
+                }
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
+
--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }: pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    (python3.withPackages (py: [
-      py.flask
-      py.requests
-    ]))
-  ];
-}
