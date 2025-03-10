d48971fb07e4061543599e8922ccbc9abd0b628a
Random936 <randomdude936@gmail.com>
2025-03-09 21:21:10 -0700

    Added static image endpoint

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import re
 import json
 from dotenv import load_dotenv
-from flask import Flask, request
+from flask import Flask, request, send_file
 from databases.mongo_db import MongoDB
 from config import MONGO_DB_HOST, MONGO_DB_PORT
 from crawlers.fda_crawler import FDAWebCrawler
@@ -13,6 +13,9 @@ app = Flask(__name__)
 
 database = MongoDB(MONGO_DB_HOST, MONGO_DB_PORT)
 database.update(FDAWebCrawler(os.getenv("FDA_API_KEY"), limit=1000), [UPCParser()])
+
+def valid_recall_number(num):
+    return re.fullmatch(r'[fF]-\d+-\d+', num)
 
 @app.route('/')
 def root_path():
@@ -25,7 +28,7 @@ def root_path():
 
 @app.route('/query/<recall>')
 def api_query(recall: str):
-    if not re.fullmatch(r'[fF]-\d+-\d+', recall):
+    if not valid_recall_number(recall):
         return json.dumps({"error": "Invalid recall number provided"})
 
     product = database.query(recall.upper())
@@ -61,6 +64,17 @@ def api_search(field: str, term: str):
     results = database.search(field, term, offset, count, sort=sort)
     return json.dumps(results)
 
+
+@app.route('/image/<recall>')
+def api_image(recall: str):
+    if not valid_recall_number(recall):
+        return json.dumps({"error": "Invalid recall number provided"})
+
+    product = database.query(recall.upper())
+    if product is None:
+        return json.dumps({"error": "Recall number not found"})
+
+    return send_file('images/static.jpg')
 
 
 if __name__ == '__main__':
